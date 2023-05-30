547432e5926ab9e3406c9fac5b06fd28b7021b62
ikocx-to24 <knru24@gmail.com>
2023-05-30 18:23:02 -0500

    did work up to question 1

--- index.js
+++ index.js
@@ -5,48 +5,71 @@ import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 
+//Plays when game opens
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms));
 
-const AsciiArt = `
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXXNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNOc::;cxXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNkc,cxxdl,:xKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNOc,cx00O00kl;:xKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMWNOc;ck0000O00O0Ol,;xXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMWWNOc,cx00O000000O00Okl;:xKNWWMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMWNOc,ck000000000OO00O000kl,:xKNWWMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMWNOc,ck000000000000000000000kl,:xKNWMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMWNOc,ck00OOOOOOOOOOOOOOOOOOOOOO0Ol,:xKNWMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMWWNOc,cxO0Ol,:c:;,;:c::c:::::::::;lO0kl,:xKNWWWMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMWNOc,ck00O0k,'okk; ,xOkkkkkkkkkkkk;'x000kl;:xKNWWMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMWNOc,ck000000d.,dlc. .lk000000000000:.o00000kl,:xKNWMMMMMMMMMMMMMM
-MMMMMMMMMMMWNOc,ck0000000O: ..      .',,,,,,,,,,,. :O0000O0Ol,:xKNWMMMMMMMMMMMM
-MMMMMMMMWWNOc,cx00000O00Oc.                        .cO0000000kl;:xKNWWMMMMMMMMM
-MMMMMMMWNOc,ck00000OO000k,                          ,k0OO000000kl,:xKWWWMMMMMMM
-MMMMMWNOc,cx00000000OOO0k,                          'k00000000000kl;:xKNWMMMMMM
-MMWWNkc,ck00000O00000000x'                          .d0O0000O0000O0Ol,:xXNWWWMM
-WWNOc,cx00O00000O0000000kl,.  .;;;;;;;;;;;;;::;.   'lk000000000000000kl;:xKNWWM
-W0c,ck000000000O0000Okdolc,. .c00000000OOkxdol:.  ,x000000000000O000000kl,:kXWM
-Xc.o0000000000000kl;',,,;:cloxO00000Od:,'',,;;:lodk0000OO0000000000000000x';OXW
-Nk:,lk0000000000d'.;oxkO00000000000Oc..;cxkO000000O00000000000000000OO0Oo;;d0XN
-WNKx:,lk0O000000l..oO000OOO00000000k; ;dk00000O000000O000000000000000Oo;;oOKXWW
-MMWNXx:;lk0O0000Ol'.';loxkO000000O00x;...,cldkO00000000000000000000Oo;;oOKXNWWW
-WWWMWNXx:;lk0OO000Oxl:;''',;coxO000000kxdl:,''',:cokO000000000000Oo;;oOKXNWWWMM
-MMMMMWWWKx:;lk000000000Oxdlc;'.'ck000000000Okxol:,'.,oO0000O0O0Oo;;oOKXNWMMMMMM
-MMMMMMMMWNKx:;lk0O00000000000Ox;.;k00OO0000000000Oko..c0000O0Oo;;oOKXNWWMMMMMMM
-MMMMMMMMMWWWXd:;lk0O000000OO00Oc 'k0O00O000000000O0x' cO000Oo;;oOKXNWWMMMMMMMMM
-MMMMMMMMMWWMWNKx:;lk00O0000O0kc..o000O000000000000d'.;k00Oo;:oOKXNWWMMMMMMMMMMM
-MMMMMMMMMMMMMMWNXx:,lk00000kl'.,dO00000000000000x:..lO0Oo;;oOKXNWMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMWNXx:,lk000x:;oO0000000000000000o,:x0Oo;;oOKXNWWMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMWWNKx:,lk00O0000000000000000000OO0Oo;;oOKXNWMWMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMWWMWNKx:,lk0O0000000000000000000Oo;;oOKXNWWWMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMWWKx:,ck000O000000000O0O0Oo;;oOKXNWMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMWNKx:,lk0O00000O00000Oo;;oOKXNWWMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMWWNKx:,lk0O0000000Oo;;oOKXNWMWMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMWWMWNKx:;lk00O00Oo;:oOKXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKx:,lkOxo;;oOKXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKx:::;:oOKXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX000KXXNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMWNNNNWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-`;
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Who wants to be a Millionaire (Streets Edition)'
+    );
 
-console.log(AsciiArt)
+    await sleep();
+    rainbowTitle.stop();
+
+    console.log(`
+      ${chalk.bgGreen('HOW TO PLAY')}
+      I am a game on your computer.
+      Just choose the correct answer using the arrow keys and press enter.
+      If you get any questions wrong I will be ${chalk.bgRed('killed')}
+      So please get it all right....
+
+    `);
+}
+await welcome()
+
+//Asking the player name
+let playerName;
+
+async function askName(){
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'What is your name?',
+        default(){
+            return 'Player';
+        },
+    });
+}
+await askName()         //Add 'Welcome to "Game title Player" later
+
+//Corrert or Incorrect Answer
+async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Checking answer...')
+    await sleep();
+
+    if (isCorrect) {
+        spinner.success({ text: `Nice work ${playerName}.`});
+    }else{
+        spinner.error({ text: `💀💀💀 Game over, you lose ${playerName}!`})         //Perhaps tell them the correct answer?
+        process.exit(1);
+    }
+}
+
+//Question 1
+async function question1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'You approach a crosswalk, and the pedestrian signal turns green. What should you do?',
+        choices: [
+            'a) Slow down and proceed with caution.',
+            'b) Speed up to cross the intersection before pedestrians.',
+            'c) Stop and allow pedestrians to cross.',
+        ],
+    });
+    return handleAnswer(answers.question_1 == 'c) Stop and allow pedestrians to cross.');
+}
+await question1()
+
+
+
